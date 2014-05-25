6bd8d0486679d3a0c90b709b5e7aa4cc74f9cedf
Zack Chen <zack.m.chen@gmail.com>
2014-05-25 15:46:48 +0800

    Assignment 2. Implements a cached matrix solver

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	
+	r <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(inverse){
+		r <<- inverse
+	}
+	getInverse <- function() r
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		r <- x$getInverse()
+		if(!is.null(r)){
+			message("getting cached data")
+			return(r)
+		}
+		
+		data <- x$get()
+		r <- solve(data,...)
+		x$setInverse(r)
+		r
 }
